c183b340938b791c1d7a852c9796349e68bea8f2
Najul1a <anageremiias@gmail.com>
2024-08-27 11:55:14 -0300

    iniciando MVC com listagem de alunos

---
+++ MVC/Controllers/AlunoController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC.Models;
+
+namespace MVC.Controllers
+{
+    public class AlunoController : Controller
+    {
+        public IActionResult Index()
+        {
+            Aluno a1 = new Aluno(1, "Ana Geremias", "anageremiias@gmail.com", 3492);
+
+            Aluno a2 = new Aluno(2, "Ana Clara", "anaclara@gmail.com", 3489);
+
+            Aluno a3 = new Aluno(3, "Livia Solla", "liviasolla@gmail.com", 3689);
+
+            Aluno a4 = new Aluno(4, "Beatriz Nascimento", "beanascimento@gmail.com", 4503);
+
+            Aluno a5 = new Aluno(5, "Mariano Dias", "marianodias@gmail.com", 5478);
+
+
+            List<Aluno> listaAluno = new List<Aluno>();
+            listaAluno.Add(a1);
+            listaAluno.Add(a2);
+            listaAluno.Add(a3);
+            listaAluno.Add(a4);
+            listaAluno.Add(a5);
+
+
+
+            return View();
+        }
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ MVC/Models/Aluno.cs
@@ -0,0 +1,25 @@
+﻿namespace MVC.Models
+{
+    public class Aluno
+    {
+        public int AlunoId { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public int RM { get; set; }
+
+
+
+
+        //Modificar o método construtor
+        //Inseriri os dados que vieram por parametro no método contrutor nos atributos da classe
+
+        public Aluno(int id, string nome, string email, int rm)
+        {
+            this.AlunoId = id;
+            this.Nome = nome;
+            this.Email = email;
+            this.RM = rm;
+        }
+
+    }
+}
